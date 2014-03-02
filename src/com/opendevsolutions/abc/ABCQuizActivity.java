92d7c7f0bb248b4dcc636a909e64c7d72e3f7e02
alpolinar <alpolinar@gmail.com>
2014-03-02 20:47:43 +0800

    updates

--- src/com/opendevsolutions/abc/ABCQuizActivity.java
+++ src/com/opendevsolutions/abc/ABCQuizActivity.java
@@ -7,7 +7,9 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -17,12 +19,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.opendevsolutions.braingain.R;
+import com.opendevsolutions.quiz.lib.QuizEvaluation;
 import com.opendevsolutions.quiz.lib.QuizItems;
 import com.opendevsolutions.quiz.lib.XmlParser;
 
 public class ABCQuizActivity extends Activity implements OnClickListener {
 
 	public QuizItems qis = new QuizItems();
+	public QuizEvaluation eval = new QuizEvaluation();
 	public static int currentItem = 0;
 	public ArrayList<String> choices = new ArrayList<String>();
 
@@ -36,6 +40,9 @@ public class ABCQuizActivity extends Activity implements OnClickListener {
 	private static String dialogTitle = "Alpabhet Quiz Results";
 	int itemsLimit = 5;
 	int correctAnswers = 0;
+
+	public AlertDialog resultDialog;
+	public AlertDialog.Builder resultBuilder;
 
 	@Override
 	public void onBackPressed() {
@@ -55,9 +62,10 @@ public class ABCQuizActivity extends Activity implements OnClickListener {
 
 	@Override
 	public void onClick(View v) {
-		Toast.makeText(this, "Please finish the Quiz first.", Toast.LENGTH_LONG).show();
+		Toast.makeText(this, "Please finish the Quiz first.", Toast.LENGTH_LONG)
+				.show();
 	}
-	
+
 	private String getChoiceFromRadioButton() {
 		for (int i = 0; i < rbtn.length; i++) {
 			if (rbtn[i].isChecked()) {
@@ -97,6 +105,22 @@ public class ABCQuizActivity extends Activity implements OnClickListener {
 	private void setLayout() {
 		tvQ.setText((currentItem + 1) + ") " + qis.getQuestion(currentItem));
 		this.addRadioButtons();
+	}
+
+	public void showResult() {
+		resultBuilder = new AlertDialog.Builder(this);
+		resultBuilder.setTitle("SCORE");
+		resultBuilder.setIcon(R.drawable.ic_launcher);
+		resultBuilder.setMessage("Score: " + correctAnswers + "/" + itemsLimit
+				+ "\n" + eval.evaluate(correctAnswers, itemsLimit));
+		resultBuilder.setPositiveButton("OK",
+				new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						
+					}
+				});
+
 	}
 
 	public static String getFileName() {
